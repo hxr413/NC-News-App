2f448e3d2636236098cadaedcb8b7fab4cf4cfcd
Riley Hu <hxr413@gmail.com>
2023-09-05 12:33:35 +0100

    task8

---
+++ src/contexts/User.jsx
@@ -0,0 +1,13 @@
+import { createContext, useState } from "react";
+
+export const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState("jessjelly");
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
